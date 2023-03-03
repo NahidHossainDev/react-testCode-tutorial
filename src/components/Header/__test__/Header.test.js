10390235a9e69d9c3d6bcf8b0dc34ccea4db96e7
nahid9764 <nahidhossain9764@gmail.com>
2023-03-03 22:38:57 +0600

    first commit

---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,53 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Header";
+
+test("Should render same text passed into title prop", async () => {
+	const title = "My Header";
+	render(<Header title={title} />);
+	const headingElement = screen.getByText(title);
+	expect(headingElement).toBeInTheDocument();
+});
+
+test("getByRole-heading", async () => {
+	const title = "My Header";
+	render(<Header title={title} />);
+	const headingElement = screen.getByRole("heading", { name: title });
+	expect(headingElement).toBeInTheDocument();
+});
+
+test("getByRole-heading should have two element", async () => {
+	const title = "My Header";
+	render(<Header title={title} />);
+	const headingElements = screen.getAllByRole("heading");
+	expect(headingElements.length > 1).toBe(true);
+});
+
+it("getByTitle-heading should have title attribute", async () => {
+	const title = "My Header";
+	render(<Header title={title} />);
+	const headingElement = screen.getByTitle(title);
+	expect(headingElement).toBeInTheDocument();
+});
+
+it("getById-header-1 should have data-test-id", async () => {
+	const title = "My Header";
+	render(<Header title={title} />);
+	const headingElement = screen.getByTestId("header-1");
+	expect(headingElement).toBeInTheDocument();
+});
+
+// FIND BY
+test("findBy should render same text", async () => {
+	const title = "My Header";
+	render(<Header title={title} />);
+	const headingElement = await screen.findByText(title);
+	expect(headingElement).toBeInTheDocument();
+});
+
+// Fuery BY
+test("queryBy not to be have the same text", async () => {
+	const title = "My Header";
+	render(<Header title={title} />);
+	const headingElement = screen.queryByText("Something ");
+	expect(headingElement).not.toBeInTheDocument();
+});
